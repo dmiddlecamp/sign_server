cc85851779c0eb59176d1de16dadb48fdd1bb1d5
David Middlecamp <dmiddlecamp@azavea.com>
2012-03-07 12:45:44 -0500

    tweaks

--- py/webapp/sign_server/views.py
+++ py/webapp/sign_server/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    return HttpResponse("Hello World")
--- py/webapp/urls.py
+++ py/webapp/urls.py
@@ -9,9 +9,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'webapp.views.home', name='home'),
     # url(r'^webapp/', include('webapp.foo.urls')),
 
+    (r'^hello_world.txt/$', 'sign_server.views.hello_world'),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
