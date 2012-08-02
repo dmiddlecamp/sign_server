5b145bf47baf335df58a637a9818752da3024857
Robert Nelson <robert.nelson@stateless-software.com>
2012-08-01 20:10:54 -0500

    Fixed url mapping.

--- py/webapp/urls.py
+++ py/webapp/urls.py
@@ -46,7 +46,7 @@ urlpatterns = patterns('',
     url(r'^peggy/2/renew_lease/(?P<lease_code>)/(?P<term>)', 'sign_server.peggy2.renew_lease'),
     url(r'^peggy/2/expire_lease/(?P<lease_code>)', 'sign_server.peggy2.expire_lease'),
     url(r'^peggy/2/clear/(?P<lease_code>.*)', 'sign_server.peggy2.clear_board'),
-    url(r'^peggy/2/write/(?P<lease_code>.*)/(?P<row>.*)/(?P<col>.*)/(?P<msg>.*)', 'sign_server.peggy2.write'),
+    url(r'^peggy/2/write/(?P<lease_code>.*)/(?P<row>.*)/(?P<col>.*)/(?P<msg>.*)', 'sign_server.peggy2.write_to_board'),
     url(r'^peggy/2/set_color/(?P<lease_code>.*)/(?P<color>.*)', 'sign_server.peggy2.set_color'),
 
 
