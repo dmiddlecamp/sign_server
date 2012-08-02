54bda546c15995d1a425ffb646c5878d4685237a
Zachary Crockett <towynlin@gmail.com>
2012-08-01 20:07:47 -0500

    Add get_lease default urls

--- py/webapp/urls.py
+++ py/webapp/urls.py
@@ -42,6 +42,8 @@ urlpatterns = patterns('',
     url(r'^peggy/set_color$', 'sign_server.peggy.set_color'),
     url(r'^peggy/set_color/(?P<lease_code>.*)/(?P<color>.*)', 'sign_server.peggy.set_color'),
 
+    url(r'^peggy/2/get_lease', 'sign_server.peggy2.get_lease'),
+    url(r'^peggy/2/get_lease/(?P<term>.*)', 'sign_server.peggy2.get_lease'),
     url(r'^peggy/2/get_lease/(?P<term>.*)/(?P<top_row>.*)/(?P<left_col>.*)/(?P<bottom_row>.*)/(?P<right_col>.*)', 'sign_server.peggy2.get_lease'),
     url(r'^peggy/2/renew_lease/(?P<lease_code>)/(?P<term>)', 'sign_server.peggy2.renew_lease'),
     url(r'^peggy/2/expire_lease/(?P<lease_code>)', 'sign_server.peggy2.expire_lease'),
