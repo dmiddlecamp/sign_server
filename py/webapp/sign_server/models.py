9407d1dbea1f5c3d524eb9a80f647a197f544f06
Robert Nelson <robert.nelson@stateless-software.com>
2012-08-01 19:43:38 -0500

    Added new lease columns.

--- py/webapp/sign_server/models.py
+++ py/webapp/sign_server/models.py
@@ -17,6 +17,10 @@ class BoardLease(models.Model):
     board_lease_code = models.CharField(max_length=32)
     is_active = models.BooleanField()
     current_color = models.TextField(max_length=1, default=chr(29))
+    top_row = models.IntegerField
+    left_col = models.IntegerField
+    bottom_row = models.IntegerField
+    right_col = models.IntegerField
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     creation_date = models.DateTimeField()
